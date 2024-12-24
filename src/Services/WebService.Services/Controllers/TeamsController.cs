57dba7fee7863012181cf1e7bb5d5ff7a558ab5b
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2024-12-24 15:41:47 +0530

    added a teams service

---
+++ src/Services/WebService.Services/Controllers/TeamsController.cs
@@ -0,0 +1,19 @@
+﻿namespace WebService.Services.Controllers;
+
+
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TeamsController : ControllerBase
+{
+
+    [HttpGet(Name = "GetTeams")]
+    public async Task<IActionResult> GetAsync()
+    {
+
+        var content = await System.IO.File.ReadAllTextAsync("Data/team.json");
+
+        return Ok(content);
+    }
+}
