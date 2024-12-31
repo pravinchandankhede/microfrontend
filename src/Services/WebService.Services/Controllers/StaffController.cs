219f921cec6da3b0ff82c7f9d498d79130f15562
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2024-12-31 12:06:06 +0530

    added a staff service

---
+++ src/Services/WebService.Services/Controllers/StaffController.cs
@@ -0,0 +1,17 @@
+﻿namespace WebService.Services.Controllers;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StaffController : ControllerBase
+{
+	[HttpGet(Name = "GetStaff")]
+	public async Task<IActionResult> GetAsync()
+	{
+		var content = await System.IO.File.ReadAllTextAsync("Data/staff.json");
+
+		return Ok(content);
+	}
+}
---
+++ src/Services/WebService.Services/Models/Staff.cs
@@ -0,0 +1,12 @@
+﻿namespace WebService.Services.Models;
+
+public class Staff
+{
+	public int StaffId { get; set; }
+	public string Name { get; set; }
+	public string Designation { get; set; }
+	public string Company { get; set; }
+	public string Contact { get; set; }
+	public string Email { get; set; }
+	public string Country { get; set; }
+}
