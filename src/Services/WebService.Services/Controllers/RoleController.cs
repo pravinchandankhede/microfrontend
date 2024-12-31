87e465ee0d1ca15241115097c434a80944377783
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2024-12-31 13:20:44 +0530

    added role and features

---
+++ src/Services/WebService.Services/Controllers/RoleController.cs
@@ -0,0 +1,16 @@
+﻿namespace WebService.Services.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RoleController : ControllerBase
+{
+	[HttpGet(Name = "GetRole")]
+	public async Task<IActionResult> GetAsync()
+	{
+		var content = await System.IO.File.ReadAllTextAsync("Data/role.json");
+
+		return Ok(content);
+	}
+}
---
+++ src/Services/WebService.Services/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿
+public class Role
+{
+	public Int32 RoleId { get; set; }
+	public String Name { get; set; }
+	public Feature[] Features { get; set; }
+}
+
+public class Feature
+{
+	public Int32 FeatureId { get; set; }
+	public String Name { get; set; }
+	public String ModuleName { get; set; }
+}
+
