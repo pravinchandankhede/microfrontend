6c953e5835fe65326cfd54d69a721b88dcde05d4
Pravin Chandankhede <pravinchandankhede@hotmail.com>
2024-12-31 14:18:32 +0530

    added the role and feature endpoints

--- src/Services/WebService.Services/Controllers/RoleController.cs
+++ src/Services/WebService.Services/Controllers/RoleController.cs
@@ -7,10 +7,34 @@ using Microsoft.AspNetCore.Mvc;
 public class RoleController : ControllerBase
 {
 	[HttpGet(Name = "GetRole")]
-	public async Task<IActionResult> GetAsync()
+	public async Task<IActionResult> GetAsync(String? roleName)
 	{
 		var content = await System.IO.File.ReadAllTextAsync("Data/role.json");
+		var roles = System.Text.Json.JsonSerializer.Deserialize<List<Role>>(content);
 
-		return Ok(content);
+		IEnumerable<Role>? result = null;
+
+		if (String.IsNullOrEmpty(roleName))
+			result = roles;
+		else
+			result = roles!.Where(m => m.Name.Equals(roleName)).ToList();
+
+		if (result!.Count() >= 0)
+		{
+			content = await System.IO.File.ReadAllTextAsync("Data/features.json");
+			var features = System.Text.Json.JsonSerializer.Deserialize<List<Feature>>(content);
+
+			foreach (var role in result!)
+			{
+				foreach (var feature in role.Features)
+				{
+					var f = features!.Where(m => m.FeatureId == feature.FeatureId).FirstOrDefault();
+					feature.Name = f.Name;
+					feature.ModuleName = f.ModuleName;
+				}
+			}
+		}
+
+		return Ok(result);
 	}
 }
